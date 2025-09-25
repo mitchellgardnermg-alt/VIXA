58870fcc6656a2a10ffe71457028548097a4d521
mitchell gardner <mitchellgardner@mitchells-MacBook-Pro.local>
2025-09-25 21:43:02 +0100

    Revert Supabase client to hardcoded values to fix deployment error

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+const supabaseUrl = 'https://yzgvmnicbcltyqaorpua.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl6Z3ZtbmljYmNsdHlxYW9ycHVhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg4MjA0MzQsImV4cCI6MjA3NDM5NjQzNH0.WTsOYYlgfDNZJaFyU9zJSg6xjwiNmCTL4esRlUU2qJM'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
